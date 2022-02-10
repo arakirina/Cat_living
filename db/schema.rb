7955cd6cbb1f06c04120a5cea1a1b490df79bd01
arakirina <arakipomu1206@gmail.com>
2022-02-10 08:05:59 +0000

    [add]viewページの記述

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_082656) do
+ActiveRecord::Schema.define(version: 2022_02_09_114150) do
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
