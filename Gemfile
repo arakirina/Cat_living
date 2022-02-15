c69ba31b3bf6ac6d7cc7362d147670c50230c446
arakirina <arakipomu1206@gmail.com>
2022-02-15 04:31:33 +0000

    [fix]

--- Gemfile
+++ Gemfile
@@ -78,3 +78,7 @@ gem "jquery-rails"
 gem 'font-awesome-sass', '~> 5.13'
 
 gem 'kaminari','~> 1.2.1'
+
+gem 'rubocop', require:false
+gem 'rubocop-rails', require:false
+gem 'rubocop-performance', require:false
--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
   devise_for :users
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root to:'homes#top'
+  get '/about' => 'homes#about'
 
   resources :categories,only: [:index]
   resources :users, only: [:show]
   resources :posts do
-    resource :favorites, only:[:create,:destroy]
-    resources :post_comments, only: [:create,:destroy]
+    resource :favorites, only:[:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
   end
-  
-  root to:'homes#top'
-  get '/about' => 'homes#about'
 
 end
